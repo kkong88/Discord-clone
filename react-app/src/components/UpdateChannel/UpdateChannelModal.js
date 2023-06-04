1555e87d0abc249b262a199a7d6020598fe83406
Kelly Kong <kkong1992@gmail.com>
2023-06-04 19:01:44 -0400

    updated modal

--- react-app/src/components/UpdateChannel/UpdateChannelModal.js
+++ react-app/src/components/UpdateChannel/UpdateChannelModal.js
@@ -1,19 +1,22 @@
-import { getOneChannel } from "../../store/channels";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import OpenModalButton from "../OpenModalButton";
 import UpdateChannel from ".";
+
 const UpdateChannelModal = ({ channel, user }) => {
   const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
+  const currentChannel = useSelector((state) => state.serversReducer.currentChannel);
 
-  
+  // Check if the user is on the current channel
+  const isUserOnChannel = currentChannel && currentChannel.id === channel.id;
+
   return (
     <>
-    <OpenModalButton
-    buttonText='Update a Channel'
-    modalComponent={<UpdateChannel />}
-    />
+        <OpenModalButton
+          buttonText="Edit Channel"
+          modalComponent={<UpdateChannel channel={channel} user={user} />}
+        />
     </>
   );
 };
--- react-app/src/store/servers.js
+++ react-app/src/store/servers.js
@@ -1,226 +1,4 @@
 import { setChannels } from "./channels";
-// const SET_SERVERS = 'servers/SetServers'
-// const REMOVE_ALL_SERVERS = 'logout/RemoveServers'
-// const USER_SERVERS = 'server/UserServers'
-// const ADD_USER_SERVERS = 'servers/AddServers'
-// const EDIT_USER_SERVERS = 'servers/EditServers'
-// const REMOVE_USER_SERVERS = 'servers/RemoveServer'
-// const CURRENT_SERVER = 'servers/CurrentServer'
-// const CLEAR_SERVER = 'servers/ClearServer'
-// const EDIT_CURRENT_SERVER = 'servers/EditCurrentServer'
-// const ADD_MEMBER_TO_SERVER = "server/AddMember"
-// const REMOVE_MEMBER_FROM_SERVER = "server/RemoveMember"
-
-
-// // All Server
-// export const setServers = (servers) => {
-//     return {
-//         type: SET_SERVERS,
-//         servers
-//     }
-// }
-
-// export const allServers = () => async (dispatch) => {
-//     const result = await fetch('/api/servers')
-//     const all_servers = await result.json()
-
-//     dispatch(allServers(all_servers.servers))
-//     return all_servers.servers
-// }
-
-// export const leftServers = () => {
-//     return {
-//         type: REMOVE_ALL_SERVERS,
-//     }
-// }
-
-// export const user_server = (server) => {
-//     return {
-//         type: USER_SERVERS,
-//         server
-//     }
-// }
-
-// export const addUserServer = (server) => async (dispatch) => {
-//     return {
-//         type: ADD_USER_SERVERS,
-//         server
-//     }
-// }
-
-// export const joinServer = (serverId, userId) => async (dispatch) => {
-//     const result = await fetch(`/api/servers/${serverId}/members/`,{
-//         method: 'POST',
-//         body: JSON.stringify({serverId, userId})
-//     })
-
-//     const data = await result.json()
-//     dispatch(joinServer(data.server))
-//     return data.server
-// }
-
-// export const leaveServer =  (serverId,memberId) => async (dispatch) => {
-//     const result = await fetch(`/api/servers/${serverId}/members/${memberId}`,{
-//         method: 'DELETE'
-//     })
-
-//     const data = await result.json()
-//     dispatch(leaveServer(data.serverId))
-// }
-
-// export const UserCreatedServer = (formData) => async (dispatch) => {
-//     const res = await fetch("/api/servers/", {
-//       method: "POST",
-//       body: formData,
-//     });
-
-//     const newServer = await res.json();
-//     dispatch(addUserServer(newServer));
-//     return newServer;
-//   };
-
-//   export const editUserServer = (server) => {
-//       return {
-//           type: EDIT_USER_SERVERS,
-//           server
-//         }
-//     }
-
-//     export const removeUserServer = (serverId) => {
-//         return {
-//             type: REMOVE_USER_SERVERS,
-//             serverId
-//         }
-//     }
-
-//     export const deleteServer = (serverId) => async (dispatch) => {
-//         await fetch(`/api/servers/${serverId}`, {
-//             method: "DELETE"
-//         })
-//         dispatch(removeUserServer(serverId))
-//     }
-
-// //Single Server
-// export const currentServer = (server) => {
-//     return {
-//         type: CURRENT_SERVER,
-//         server
-//     }
-// }
-
-// export const clearServer = () => {
-//     return {
-//         type: CLEAR_SERVER
-//     }
-// }
-
-// export const serverDetail = (serverId) => async (dispatch) => {
-//     const result = await fetch(`/api/servers/${serverId}`)
-//     const singleServer = await result.json()
-//     dispatch(currentServer(singleServer))
-//     dispatch(setChannels(singleServer.channels))
-//     return singleServer
-// }
-
-// export const updateServer = (server) => {
-//     return {
-//         type: EDIT_CURRENT_SERVER,
-//         server
-//     }
-// }
-
-// export const updateCurrentServer = (serverId, formData) => async (dispatch) => {
-//     const result = await fetch(`/api/servers/${serverId}`, {
-//         method: 'PUT',
-//         body: formData
-//     })
-//     const update = await result.json()
-//     dispatch(updateCurrentServer(update))
-//     dispatch(editUserServer(update))
-//     dispatch(setChannels(update))
-
-//     return update.picture
-// }
-
-// export const addMember = (serverId, member) => {
-//   return { type: ADD_MEMBER_TO_SERVER, serverId, member };
-// };
-
-// export const removeMember = (serverId, memberId) => {
-//   return { type: REMOVE_MEMBER_FROM_SERVER, serverId, memberId };
-// };
-
-// const initialState = {
-//     userServers: { server: null },
-//     allServers: { server: null },
-//     currentServer: {
-//       channels: { 0: 0 },
-//       members: { 0: 0 },
-//     },
-//   };
-
-//   const serversReducer = (state = initialState, action) => {
-//     let newState = { ...state };
-//     switch (action.type) {
-//       case REMOVE_ALL_SERVERS: {
-//         return initialState;
-//       }
-
-//       case CURRENT_SERVER: {
-//         newState.currentServer = initialState.currentServer;
-//         return newState;
-//       }
-//       case SET_SERVERS: {
-//         newState.allServers = action.servers;
-//         return newState;
-//       }
-//       case USER_SERVERS: {
-//         newState.userServers = action.servers;
-//         return newState;
-//       }
-
-//       case EDIT_USER_SERVERS: {
-//         newState.userServers[action.server.id] = action.server;
-//         return newState;
-//       }
-
-//       case ADD_USER_SERVERS: {
-//         newState.userServers[action.server.id] = action.server;
-//         return newState;
-//       }
-
-//       case REMOVE_USER_SERVERS: {
-//         delete newState.userServers[action.serverId];
-//         return newState;
-//       }
-
-//       case CURRENT_SERVER: {
-//         newState.currentServer = action.server;
-//         return newState;
-//       }
-
-//       case EDIT_CURRENT_SERVER: {
-//         newState.currentServer = action.server;
-//         return newState;
-//       }
-
-//       case ADD_MEMBER_TO_SERVER: {
-//         newState.currentServer.members[action.member.id] = action.member;
-//         return newState;
-//       }
-
-//       case REMOVE_MEMBER_FROM_SERVER: {
-//         delete newState.currentServer.members[action.memberId];
-//         return newState;
-//       }
-
-//       default:
-//         return state;
-//     }
-//   };
-
-//   export default serversReducer;
-
 
 const SET_ALL_SERVERS = "servers/SetAllServers";
 const SET_USER_SERVERS = "servers/SetUserServers";
@@ -233,12 +11,11 @@ const UPDATE_CURRENT_SERVER = "currentServer/UpdateSelectedServer";
 const ADD_MEMBER_TO_SERVER = "currentServer/AddMember";
 const REMOVE_MEMBER_FROM_SERVER = "currentServer/RemoveMember";
 
-// adds servers to the store
+
 export const setAllServers = (servers) => {
   return { type: SET_ALL_SERVERS, servers };
 };
 
-// gets all the servers in the back end to pass off to setAllServers
 export const getAllServers = () => async (dispatch) => {
     const res = await fetch("/api/servers");
     const servers = await res.json();
@@ -248,7 +25,7 @@ export const getAllServers = () => async (dispatch) => {
   };
 
 
-// sets servers the user belongs in the store
+
 export const setUserServers = (servers) => {
     return {
       type: SET_USER_SERVERS,
@@ -257,14 +34,14 @@ export const setUserServers = (servers) => {
   };
 
 
-// adds a server the user joined in the store
+
 export const addUserServer = (server) => {
     return {
       type: ADD_USER_SERVER,
       server
     };
   };
-// adds user to an existing server they just joined
+
 export const joinUserServer = (serverId, userId) => async (dispatch) => {
 
   const res = await fetch(`/api/servers/${serverId}/members`, {
@@ -278,7 +55,7 @@ export const joinUserServer = (serverId, userId) => async (dispatch) => {
     return data.server;
   };
 
-  // adds new server the user has created to the store
+
   export const postUserServer = (formData) => async (dispatch) => {
       const res = await fetch("/api/servers", {
         method: "POST",
@@ -300,12 +77,11 @@ export const joinUserServer = (serverId, userId) => async (dispatch) => {
   //   return newServer;
   // };
 
-// remove a server the user joined in the store
+
 export const removeUserServer = (serverId) => {
       return { type: REMOVE_USER_SERVER, serverId };
     };
 
-// deletes the user from a server they joined
 export const leaveUserServer = (serverId, membershipId) => async (dispatch) => {
     const res = await fetch(
       `/api/servers/${serverId}/members/${membershipId}`,
@@ -316,7 +92,7 @@ export const leaveUserServer = (serverId, membershipId) => async (dispatch) => {
     const data = await res.json();
     dispatch(removeUserServer(data.serverId));
   };
-// deletes server entitely, user must own the server
+
 export const deleteServer = (serverId) => async (dispatch) => {
     await fetch(`/api/servers/${serverId}`, {
       method: "DELETE",
@@ -325,7 +101,7 @@ export const deleteServer = (serverId) => async (dispatch) => {
   };
 
 
-// setting the selected server to the store and removing it when no longer selected
+
   export const setCurrentServer = (server) => {
     return { type: SET_SELECTED_SERVER, server };
   };
@@ -343,7 +119,6 @@ export const getAServer = (serverId) => async (dispatch) => {
   };
 
 
-// updating the selected server in store for both "current" and servers you are a member of
   export const updateCurrentServer = (server) => {
     return { type: UPDATE_CURRENT_SERVER, server };
   };
@@ -366,7 +141,6 @@ export const getAServer = (serverId) => async (dispatch) => {
     // return updatedServer.picture;
   };
 
-//adding or deleting users from the members
 export const addMember = (serverId, member) => {
     return { type: ADD_MEMBER_TO_SERVER, serverId, member };
   };
