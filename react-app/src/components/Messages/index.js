5917899431ee1b155f9ebdc5533cb33a18d3cce7
Kelly Kong <kkong1992@gmail.com>
2023-07-22 14:12:31 -0400

    fixed styling issues with messages after some change

--- react-app/src/components/Messages/index.js
+++ react-app/src/components/Messages/index.js
@@ -13,64 +13,59 @@ const Messages = ({ messages, handleDeleteMessage, handleUpdateMessage }) => {
   const user = useSelector((state) => state.session.user);
   const server = useSelector((state) => state.servers?.currentServer);
   const channel = useSelector((state) => state.channels?.currentChannel);
-//   let messagesEnd = useRef(null);
-//   useEffect(() => {
-//     let isActive = true;
-//     isActive && messagesEnd.current?.scrollIntoView();
-//     return () => {
-//       isActive = false;
-//     };
-//   }, [messages]);
-return (
-  <div className="messages">
-    <h1>Messages</h1>
-    {messages?.map((message) => (
-      <div className="message" key={message.id}>
-        <div className="message_content">
-          <div className="message_user">
-            <img
-              className="message_pfp"
-              src={message.senderProfilePicture}
-              alt="pfp"
-            />
-            <h4 className="username">{message.senderUsername}</h4>
-            {showEditMessage !== message.id && <p>{message.content}</p>}
-            {showEditMessage === message.id && (
-              <Chat
-              messageToEdit={message}
-              handleUpdateMessage={handleUpdateMessage}
-              setShowEditMessage={setShowEditMessage}
+
+  return (
+    <div className="messages">
+      <h1>Messages</h1>
+      {messages?.map((message) => (
+        <div className="message" key={message.id}>
+          <div className="message_content">
+            <div className="message_user">
+              <img
+                className="message_pfp"
+                src={message.senderProfilePicture}
+                alt="pfp"
+              />
+              <div className="message_user_info">
+                <h4 className="username">{message.senderUsername}</h4>
+                {showEditMessage !== message.id && <div>{message.content}</div>}
+                {showEditMessage === message.id && (
+                  <Chat
+                    messageToEdit={message}
+                    handleUpdateMessage={handleUpdateMessage}
+                    setShowEditMessage={setShowEditMessage}
+                  />
+                )}
+              </div>
+            </div>
+            {hover === message.id && message.senderId !== 1 && (
+              <div className="message_more">
+                <button
+                  onClick={() => (options ? setOptions(false) : setOptions(true))}
+                >
+                  {/* <img
+                    src="/svgs/dot-dot.svg"
+                    alt="more"
+                    className="delete"
+                  ></img> */}
+                </button>
+              </div>
+            )}
+            {options && message.id === hover && (
+              <MessageOptions
+                handleDeleteMessage={handleDeleteMessage}
+                message={message}
+                user={user}
+                server={server}
+                channel={channel}
+                setShowEditMessage={setShowEditMessage}
               />
             )}
           </div>
-          {hover === message.id && message.senderId !== 1 && (
-            <div className="message_more">
-              <button
-                onClick={() => (options ? setOptions(false) : setOptions(true))}
-              >
-                {/* <img
-                  src="/svgs/dot-dot.svg"
-                  alt="more"
-                  className="delete"
-                ></img> */}
-              </button>
-            </div>
-          )}
-             {options && message.id === hover && (
-            <MessageOptions
-              handleDeleteMessage={handleDeleteMessage}
-              message={message}
-              user={user}
-              server={server}
-              channel={channel}
-              setShowEditMessage={setShowEditMessage}
-            />
-          )}
         </div>
-      </div>
-    ))}
-  </div>
- );
+      ))}
+    </div>
+  );
 };
 
 export default Messages;
