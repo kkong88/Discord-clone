224fc725d3d8775208a52c343f97374c84677904
Kelly Kong <kkong1992@gmail.com>
2023-07-22 13:58:12 -0400

    updated message-option

--- react-app/src/components/Messages/index.js
+++ react-app/src/components/Messages/index.js
@@ -1,10 +1,18 @@
 import Channels from "../Channels";
 import "./Messages.css";
+import MessageOptions from "../MessageOptions";
+import Chat from "../Chat";
 
+import { useRef, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-import { useRef, useEffect } from "react";
-
-const Messages = ({ messages }) => {
+const Messages = ({ messages, handleDeleteMessage, handleUpdateMessage }) => {
+  const [hover, setHover] = useState(false);
+  const [options, setOptions] = useState(null);
+  const [showEditMessage,setShowEditMessage] = useState(false)
+  const user = useSelector((state) => state.session.user);
+  const server = useSelector((state) => state.servers?.currentServer);
+  const channel = useSelector((state) => state.channels?.currentChannel);
 //   let messagesEnd = useRef(null);
 //   useEffect(() => {
 //     let isActive = true;
@@ -26,8 +34,38 @@ return (
               alt="pfp"
             />
             <h4 className="username">{message.senderUsername}</h4>
+            {showEditMessage !== message.id && <p>{message.content}</p>}
+            {showEditMessage === message.id && (
+              <Chat
+              messageToEdit={message}
+              handleUpdateMessage={handleUpdateMessage}
+              setShowEditMessage={setShowEditMessage}
+              />
+            )}
           </div>
-          <p>{message.content}</p>
+          {hover === message.id && message.senderId !== 1 && (
+            <div className="message_more">
+              <button
+                onClick={() => (options ? setOptions(false) : setOptions(true))}
+              >
+                {/* <img
+                  src="/svgs/dot-dot.svg"
+                  alt="more"
+                  className="delete"
+                ></img> */}
+              </button>
+            </div>
+          )}
+             {options && message.id === hover && (
+            <MessageOptions
+              handleDeleteMessage={handleDeleteMessage}
+              message={message}
+              user={user}
+              server={server}
+              channel={channel}
+              setShowEditMessage={setShowEditMessage}
+            />
+          )}
         </div>
       </div>
     ))}
