e2f8bf5f2dd77aced3e8cb6aaaf01ce8a64044e7
Kelly Kong <kkong1992@gmail.com>
2023-08-25 19:14:22 -0400

    added social links into sign up and login page

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -93,6 +93,17 @@ const LoginFormPage = () => {
             </button>
           </div>
         </form>
+        <div className="social-links">
+               <a href="https://github.com/kkong88">
+                <img src="https://res.cloudinary.com/dip4w3xmy/image/upload/v1692831825/1200px-GitHub_Invertocat_Logo.svg_vhlphv.png" className="left_side_icon" />
+            </a>
+              <a href="https://www.linkedin.com/in/kelly-kong-033333186/">
+                 <img src="https://res.cloudinary.com/dip4w3xmy/image/upload/v1692831946/square-linkedin-logo-isolated-white-background_469489-892_gfubjw.png" className="left_side_icon" />
+              </a>
+                 <a href="https://wellfound.com/u/kelly-kong-1">
+               <img src="https://res.cloudinary.com/dip4w3xmy/image/upload/v1692832810/1668538387615_yvrj9w.jpg" className="left_side_icon" />
+              </a>
+          </div>
       </div>
         </div>
     )
